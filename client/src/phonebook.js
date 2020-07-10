68ae69ff762f2a5b85d6081dfd792b92bc301215
matt.outen <matt.outen@mattouten.local>
2020-07-10 19:47:37 -0400

    fix dans number

--- client/src/phonebook.js
+++ client/src/phonebook.js
@@ -8,11 +8,12 @@ module.exports = {
   Emily: {number:'+19196495660', email: 'emilygdispirito@gmail.com'},
   Eddie: { number:'+16073391244', email: 'edward61@hotmail.com'},
   Mom: { number: '+16073270061', email: 'lmcowen64@aol.com'},
-  Dan: {number: '+7138352958'},
+  Dan: {number: '+17138352958'},
   Dad: {number: '+17044415717'},
   Cait: {number: '+18327730470'},
   Bailey: {number: '+16073391078'},
   Ali: {number:'+18324998779'},
   Kev: {number: '+17042073892'},
-  Kashaunda: {number: '+17042946589'}
+  Kashaunda: {number: '+17042946589'},
+  Mitch_Martin: {number: '+17046954574'}
 };
